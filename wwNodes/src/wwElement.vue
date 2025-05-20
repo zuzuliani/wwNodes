5053c089f159b01c20d9eb655897c18ffc34f933
zuzuliani <68553057+zuzuliani@users.noreply.github.com>
2025-05-20 20:04:26 -0300

    init

---
+++ wwNodes/src/wwElement.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="my-element">
+    <p :style="textStyle">I am a custom element !</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {
+    textStyle() {
+      return {
+        color: this.content.textColor,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-element {
+  p {
+    font-size: 18px;
+  }
+}
+</style>
